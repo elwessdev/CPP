b22cb606fda25d3eaa6d6258fc20b3301657dd13
devosema <benaliosama3@gmail.com>
2024-01-13 11:53:08 +0100

    last

--- Recursive_Function.cpp
+++ Recursive_Function.cpp
@@ -41,14 +41,12 @@ int length_3n1(int n){
         return length_3n1(n/2);
     return length_3n1(3*n+1);
 }
-
 // P2- Power function
 int my_pow(int val, int p=2){
     if(p==0)
         return 1;
     return val*my_pow(val,p-1);
 }
-
 // P3- Array maximum
 int arr_max(int arr[],int len){
     if(len==1)
@@ -56,15 +54,13 @@ int arr_max(int arr[],int len){
     int sub_result=arr_max(arr,len-1);
     return max(sub_result, arr[len-1]);
 }
-
-//// P4- Array Sum
+// P4- Array Sum
 int arr_sum(int arr[],int len){
     if(len==1)
         return arr[0];
     int sub_result=arr_sum(arr,len-1);
     return sub_result+arr[len-1];
 }
-
 // P5- Array average
 float arr_average(float arr[],int len){
     if(len==1)
@@ -73,7 +69,6 @@ float arr_average(float arr[],int len){
     sub_result=sub_result*(len-1);
     return (sub_result+arr[len-1])/len;
 }
-
 // P6- Array incremen
 void arr_increment(int arr[], int len){
     if(len==0)
@@ -81,7 +76,6 @@ void arr_increment(int arr[], int len){
     arr_increment(arr,len-1);
     arr[len-1]+=len-1;
 }
-
 // P7- Array accumulation
 void accumulate_arr(int arr[],int len){
     if(len==1)
@@ -89,58 +83,83 @@ void accumulate_arr(int arr[],int len){
     accumulate_arr(arr,len-1);
     arr[len-1]+=arr[len-2];
 }
+// P7- Array accumulation
+void left_max(int arr[], int len){
+    if(len==1)
+        return;
+    left_max(arr,len-1);
+    arr[len-1]=max(arr[len-1],arr[len-2]);
+}
+
+//-------- Medium to Hard Challenges --------
+// P1 - Right Max
+void right_max(int arr[],int len,int start_position=0){
+    if(len==0)
+        return;
+    right_max(arr,len-1,start_position);
+    // arr[len]=max(arr[len],arr[len-1]);
+    cout<<arr[len]<<"\n";
+}
+// P2 - Suffix Sum
+int suffix_sum(int arr[],int len,int signature){
+    if(signature==1)
+        return arr[len-1];
+    int sub_result=suffix_sum(arr,len-1,signature-1);
+    return sub_result+arr[len-1];
+}
+
 
 
 int main(){
 //-------- Homework --------
 // Factorial with recursive
-//    cout<<factorial(6);
+/*   cout<<factorial(6);*/
 
 // Print a Triangle (v1)
-//    print_triangle(5);
+/*   print_triangle(5);*/
 
 // Print 3n+1 Sequence
-//    th_sequence(6);
+/*   th_sequence(6);*/
 
 //-------- Easy to Medium Challenges --------
 // P1- Length of 3n+1
-//    cout<<length_3n1(6);
+/*   cout<<length_3n1(6);*/
 
 // P2- Power function
-//    cout<<my_pow(5,3);
+/*   cout<<my_pow(5,3);*/
 
 // P3- Array maximum
-//    int arr[]={1,44,5,23,7};
-//    cout<<arr_max(arr,5);
+/*   int arr[]={1,44,5,23,7};
+   cout<<arr_max(arr,5);*/
 
 // P4- Array Sum
-//    int arr[100];
-//    int len; cin>>len;
-//    for(int i=0;i<len;i++){
-//        cin>>arr[i];
-//    }
-//    cout<<arr_sum(arr,len);
+/*   int arr[100];
+   int len; cin>>len;
+   for(int i=0;i<len;i++){
+       cin>>arr[i];
+   }
+   cout<<arr_sum(arr,len);*/
 
 // P5- Array average
-//     float arr[100];
-//     int len; cin>>len;
-//     for(int i=0;i<len;i++){
-//         cin>>arr[i];
-//     }
-//     cout<<arr_average(arr,len);
+/*    float arr[100];
+    int len; cin>>len;
+    for(int i=0;i<len;i++){
+        cin>>arr[i];
+    }
+    cout<<arr_average(arr,len);*/
 
 // P6- Array increment
-//    int arr[100],len; cin>>len;
-//    for(int i=0;i<len;i++){
-//        cin>>arr[i];
-//    }
-//    arr_increment(arr,len);
-//    for(int i=0;i<len;i++){
-//       cout<<arr[i]<<" ";
-//    }
+/*   int arr[100],len; cin>>len;
+   for(int i=0;i<len;i++){
+       cin>>arr[i];
+   }
+   arr_increment(arr,len);
+   for(int i=0;i<len;i++){
+      cout<<arr[i]<<" ";
+   }*/
 
 // P7- Array Accumulation
-    int arr[100];
+/*    int arr[100];
     int len; cin>>len;
     for(int i=0;i<len;i++){
         cin>>arr[i];
@@ -148,12 +167,32 @@ int main(){
     accumulate_arr(arr,len);
     for(int i=0;i<len;i++){
         cout<<arr[i]<<" ";
+    }*/
+
+// P8- Left Max
+/*    int arr[100], len; cin>>len;
+    for(int i=0;i<len;i++){
+        cin>>arr[i];
     }
+    left_max(arr,len);
+    for(int i=0;i<len;i++){
+        cout<<arr[i]<<" ";
+    }*/
 
+//-------- Medium to Hard Challenges --------
+// P1 - Right Max
+/*    int arr[] = {1,3,5,7,4,2};
+    int len=6;
+    right_max(arr,len);
+    for(int i=0;i<len;i++){
+        cout<<arr[i]<<" ";
+    }
+*/
 
-
-
-
+// P2 - Suffix Sum
+    int arr[]={1,3,4,6,7};
+    int signature=3,len=5;
+    cout<<suffix_sum(arr,len,signature);
 
 
 

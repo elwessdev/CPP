dd1ea7d46d1d93e5b211bed5cf23da9b88d8f3d7
CoderOsama <bms93951@gmail.com>
2024-01-18 11:21:44 +0100

    recursive

--- Recursive_Function.cpp
+++ Recursive_Function.cpp
@@ -93,43 +93,39 @@ void left_max(int arr[], int len){
 
 //-------- Medium to Hard Challenges --------
 // P1 - Right Max
-void right_max(int arr[],int len,int start_position=0){
-    if(len==0)
+void right_max(int arr[],int len,int start_pos=0){
+    if(start_pos==len-1)
         return;
-    right_max(arr,len-1,start_position);
-    // arr[len]=max(arr[len],arr[len-1]);
-    cout<<arr[len]<<"\n";
+    right_max(arr,len,start_pos+1);
+     arr[start_pos]=max(arr[start_pos],arr[start_pos+1]);
 }
 // P2 - Suffix Sum
 int suffix_sum(int arr[],int len,int signature){
-    if(signature==1)
-        return arr[len-1];
-    int sub_result=suffix_sum(arr,len-1,signature-1);
-    return sub_result+arr[len-1];
+    if(signature==0)
+        return 0;
+    return arr[len-1]+suffix_sum(arr,len-1,signature-1);
 }
 // P3 - Prefix Sum
 int prefix_sum(int arr[],int len,int signature){
     if(signature==0)
-        return arr[signature-1];
-    int sub_result=prefix_sum(arr,len,signature-1);
-    return sub_result+arr[signature-1];
+        return 0;
+    return arr[signature-1]+prefix_sum(arr,len,signature-1);
 }
 // P4 - Is Palindrome
-bool is_palindrome(int arr[],int len,int signature){
-    if(arr[signature]!=arr[len])
+bool is_palindrome(int arr[],int start_p,int end_p){
+    if(start_p>=end_p)
+        return true;
+    if(arr[start_p]!=arr[end_p])
         return false;
-    is_palindrome(arr,len-1,signature+1);
+    return is_palindrome(arr,start_p+1,end_p-1);
 }
 // P5 - Is Prefix
 bool is_prefix(string main,string prefix,int start_pos=0){
-    cout<<prefix.size()-1<<" "<<start_pos<<" "<<main[start_pos]<<" "<<prefix[start_pos]<<endl;
-    if(start_pos==prefix.size()-1&&main[start_pos]==prefix[start_pos]){
+    if(prefix.size()==start_pos)
         return true;
-    } else if(main[start_pos]!=prefix[start_pos]){
+    if(main[start_pos]!=prefix[start_pos])
         return false;
-    } else {
-        is_prefix(main,prefix,start_pos+1);
-    }
+    return is_prefix(main,prefix,start_pos+1);
 }
 // P6 - Trace -> This function to reverse numbers
 void do_something(int n){
@@ -138,20 +134,38 @@ void do_something(int n){
         do_something(n/10);
     }
 }
-// P7 - Count primes
-void count_primes(int p_start,int p_end){
-    if(p_start<p_end){
-        for(int i=2;i<p_start;i++){
-            if(p_start%i==0){
-                count_primes(p_start+1,p_end);
-            }
-        }
-        cout<<p_start<<" ";
-        count_primes(p_start+1,p_end);
+void do_something2(int n){
+    if(n){
+        cout<<n%10<<" ";
+        do_something(n/10);
     }
 }
+// P7 - Count primes
+int is_prime(int m,int test_num=3){
+    if(m==2)
+        return true;
+    if(m<=1||m%2==0)
+        return false;
+    if(m==test_num)
+        return true;
+    if(m%test_num==0)
+        return false;
+    return is_prime(m,test_num+1);
+}
+int count_primes(int p_start,int p_end){
+    if(p_start>p_end)
+        return 0;
+    int result=count_primes(p_start+1,p_end);
+    if(is_prime(p_start))
+        result+=1;
+    return result;
+}
 // P8 - Grid Sum
-int path_sum(int grid[][],int row,int col,int ROWS,int COLS){
+//int path_sum(int grid[][],int row,int col,int ROWS,int COLS){
+//
+//}
+// P9 - Fibonacci
+int fibonacci(int n){
 
 }
 
@@ -231,50 +245,49 @@ int main(){
 
 //-------- Medium to Hard Challenges --------
 // P1 - Right Max
-/*    int arr[] = {1,3,5,7,4,2};
+    /*int arr[] = {1,3,5,7,4,2};
     int len=6;
     right_max(arr,len);
     for(int i=0;i<len;i++){
         cout<<arr[i]<<" ";
-    }
-*/
+    }*/
 
 // P2 - Suffix Sum
-//    int arr[]={1,3,4,6,7};
-//    int signature=3,len=5;
-//    cout<<suffix_sum(arr,len,signature);
+    /*int arr[]={1,3,4,6,7};
+    int signature=3,len=5;
+    cout<<suffix_sum(arr,len,signature);*/
 
 // P3 - Prefix Sum
-//    int arr[]={1,3,4,6,7};
-//    int signature=3,len=5;
-//    cout<<prefix_sum(arr,len,signature);
+    /*int arr[]={1,2,4,6,7};
+    int signature=3,len=5;
+    cout<<prefix_sum(arr,len,signature);*/
 
 // P4 - Is Palindrome
-//    int arr[]={1,3,5,3,1};
-//    int len=5,signature=0;
-//    cout<<is_palindrome(arr,len,signature);
+    /*int arr[]={1,3,5,3,1};
+    cout<<is_palindrome(arr,0,4);*/
 
 // P5 - Is Prefix
-//    string main="abcdefgh", prefix="abcde";
-//    if(is_prefix(main,prefix)){
-//        cout<<"Yes";
-//    } else {
-//        cout<<"false";
-//    }
+    /*string main="abcdefgh", prefix="abcdse";
+    cout<<is_prefix(main,prefix);*/
 
 // P6 - Trace
-//    do_something(123456);
+    /*do_something(123456); // Print digits by reversed
+    do_something2(123456); // Print number digits*/
 
 // P7 - Count primes
-//    int p_start=10,p_end=20;
-//    count_primes(p_start,p_end);
+    /*int p_start=10,p_end=20;
+    cout<<count_primes(p_start,p_end);*/
 
 // P8 - Grid Sum
-    int grid[100][100];
-    int row,col; cin>>row>>col;
-    int ROWS,COLS;
-    cout<<path_sum(grid,row,col,ROWS,COLS);
+//    int grid[100][100];
+//    int row,col; cin>>row>>col;
+//    int ROWS,COLS;
+//    cout<<path_sum(grid,row,col,ROWS,COLS);
 
+// P9 - Fibonacci
+//    int arr[100];
+//    int n=6;
+//    cin>>fibonacci(n);
 
 
 
